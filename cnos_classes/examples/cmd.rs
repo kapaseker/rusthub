c330dadc42daf80afda7eb693bc10dab78bb3d71
xiepg <xiepg@meixing.com>
2024-11-28 09:09:51 +0800

    cmd

---
+++ cnos_classes/examples/cmd.rs
@@ -0,0 +1,15 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    println!("I'm picking a number between 1 and 100...");
+
+    print!("Enter a number: ");
+    io::stdout().flush().unwrap();
+    let mut val = String::new();
+
+    io::stdin().read_line(&mut val)
+        .expect("Error getting guess");
+
+    println!("You entered {}", val);
+}
