d517db145385e56cff39ccf06608284dbd933b4d
kapaseker <panoogunker@gmail.com>
2025-10-23 23:33:49 +0800

    new opts optimize

--- rcli/src/main.rs
+++ rcli/src/main.rs
@@ -1,14 +1,18 @@
 use clap::Parser;
-use rcli::opts::{Opt, SubCommand};
+use rcli::opts::{Opt, OutputFormat, SubCommand};
 use rcli::process::process_csv;
 
 fn main() -> anyhow::Result<()> {
+    println!("{}", OutputFormat::Json);
     let opts = Opt::parse();
     println!("command is :{:?}", opts);
 
     match opts.cmd {
         SubCommand::Csv(csv_opts) => {
-            process_csv(&csv_opts.input, &csv_opts.output)?;
+            let output_file = csv_opts
+                .output
+                .unwrap_or_else(|| format!("output.{}", csv_opts.format));
+            process_csv(&csv_opts.input, &output_file, csv_opts.format)?;
         }
     }
 
--- rcli/src/process.rs
+++ rcli/src/process.rs
@@ -1,23 +1,22 @@
+use crate::opts::OutputFormat;
 use csv::Reader;
 use serde_json::Value;
-
 // Name,Position,Jersey Number,Nationality,Age
 
-pub fn process_csv(input_file: &str, output_file: &str) -> anyhow::Result<()> {
+pub fn process_csv(
+    input_file: &str,
+    output_file: &str,
+    format: OutputFormat,
+) -> anyhow::Result<()> {
+    println!("Processing {}, {}, {:?}", input_file, output_file, format);
     let mut reader = Reader::from_path(input_file)?;
     let mut record_list = vec![];
     let header = reader.headers()?.clone();
-    // println!("{:?}", header);
     for record in reader.records() {
         let rec = record?;
         let zipped = header.iter().zip(rec.iter()).collect::<Value>();
-        // println!("{:?}", zipped);
         record_list.push(zipped);
     }
-    // let players = rdr
-    //     .deserialize::<Player>()
-    //     .map(|player| player.unwrap())
-    //     .collect::<Vec<Player>>();
     let player_json = serde_json::to_string_pretty(&record_list)?;
     std::fs::write(output_file, player_json)?;
     Ok(())
